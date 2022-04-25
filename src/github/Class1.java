62396a41c8793071d0bdf20435a526815147d9d7
omkar <omkar@LAPTOP-B0UCABTJ>
2022-04-25 15:45:53 +0530

    update

--- src/github/Class1.java
+++ src/github/Class1.java
@@ -3,5 +3,7 @@ package github;
 public class Class1 {
 public static void main(String[] args) {
 	System.out.println("Hello world");
+	System.out.println("good");
+	System.out.println("afternoon");
 }
 }
